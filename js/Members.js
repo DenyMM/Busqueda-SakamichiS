3cc57894a131de5b478df21ad59e6324f755c311
DenyMM <deny.lolipop@gmail.com>
2019-02-11 17:21:52 -0600

    Fetch

--- js/Members.js
+++ js/Members.js
@@ -1,5 +1,5 @@
-const url = ""; // site that doesn’t send Access-Control-*
-fetch(url)
+const url = "https://raw.githubusercontent.com/DenyMM/CRUD-KSakamichiS/master/webcrudbde1-Sakamichi-export.json"; 
+fetch(url).then(result => result.json())
 .then(response => response.text())
 .then(contents => console.log(contents))
 .catch(() => console.log("No puede acceder" + url + " bloqueado por exploradorS"))
