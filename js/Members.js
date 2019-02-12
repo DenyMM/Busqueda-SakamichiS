caa87fa9528bb4b3cfea6f3841d0b6890d9da750
DenyMM <deny.lolipop@gmail.com>
2019-02-12 03:43:15 -0600

    Se accede al JSON

--- js/Members.js
+++ js/Members.js
@@ -1,9 +1,20 @@
 
 
-const url = "https://raw.githubusercontent.com/DenyMM/Busqueda-SakamichiS/master/webcrudbde1-Sakamichi-export.json"; // site that doesn’t send Access-Control-*
-fetch(url)
-.then(response => response.text())
-.then(Sakamichi => document.getElementById('nombre').innerHTML = (Sakamichi))
+const url = "https://api.myjson.com/bins/mfeem"; // site that doesn’t send Access-Control-*
+fetch(url).then((res) => {
+  return res.json();
+  var Zaka = JSON.Parse(url);
+}).then((Zaka) => {
+document.getElementById('nombre').innerHTML = Object.entries(Zaka);
+console.log(Object.values(Zaka)[0]["Keyakizaka46"]["Generation1"]);
+document.getElementById('apellido').innerHTML = Object.values(Zaka)[0]["Keyakizaka46"]["Generation1"];
+}).catch((error) => {
+console.log(error);
+});
+/*fetch(url)
+.then(response => response.json())
+.then(Sakamichi => document.getElementById('nombre').innerHTML = (Object.keys(Sakamichi)))
+.then(Keyakizaka46 => console.log(Object.values(Keyakizaka46)))
 .catch(() => console.log("Can’t access " + url + " response. Blocked by browser?"))
 /*
 fetch(SakaAPI).then((res) => {
