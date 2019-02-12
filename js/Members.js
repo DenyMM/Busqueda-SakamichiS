7d637bb9ecc5f634b33cbcd5b2c31d957ffbea13
DenyMM <deny.lolipop@gmail.com>
2019-02-11 19:55:07 -0600

    Se accedio a JSON

--- js/Members.js
+++ js/Members.js
@@ -1,5 +1,25 @@
-const url = "https://raw.githubusercontent.com/DenyMM/CRUD-SakamichiS/master/webcrudbde1-Sakamichi-export.jsont ";
-fetch(url).then(result => result.json())
+
+
+const url = "https://raw.githubusercontent.com/DenyMM/Busqueda-SakamichiS/master/webcrudbde1-Sakamichi-export.json"; // site that doesn’t send Access-Control-*
+fetch(url)
 .then(response => response.text())
-.then(contents => console.log(contents))
-.catch(() => console.log("No puede acceder" + url + " bloqueado por exploradorS"))
+.then(Sakamichi => document.getElementById('nombre').innerHTML = (Sakamichi))
+.catch(() => console.log("Can’t access " + url + " response. Blocked by browser?"))
+/*
+fetch(SakaAPI).then((res) => {
+  return res.json();
+}).then((Sakamichi) => {
+  // Imprime Sedes
+  document.getElementById('nombre').innerHTML = Object.keys(Sakamichi.Keyakizaka46)[0];
+}).catch((error) => {
+  console.log(error);
+});
+/*
+fetch('https://raw.githubusercontent.com/DenyMM/Busqueda-SakamichiS/master/webcrudbde1-Sakamichi-export.json')
+    .then((respuesta) => {
+        return respuesta.json();
+    } ).then((respuesta) => {
+        document.getElementById('nombre').value = respuesta.Sakamichi;
+        document.getElementById('apellido').value = respuesta.Hinatazaka46;
+    })
+*/
