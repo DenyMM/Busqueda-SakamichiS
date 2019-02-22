43a9c8aeab1a5b6c98d4e613f2ca5493306c22f1
DenyMM <deny.lolipop@gmail.com>
2019-02-22 00:20:07 -0600

    Se accedio a todo el JSON

--- js/Hinatazaka46.js
+++ js/Hinatazaka46.js
@@ -5,9 +5,52 @@ fetch(url).then((res) => {
 }).then((Zaka) => {
 //document.getElementById('memb').innerHTML = JSON.stringify(Zaka);
 console.log(Object.values(Zaka)[0]["Keyakizaka46"]["Generation1"]);
-document.getElementById('M3a').innerHTML = Object.values(Zaka)[0]["Hinatazaka46"]["Generation1"]["HSMM"];
-document.getElementById('M3b').innerHTML = Object.values(Zaka)[0]["Hinatazaka46"]["Generation2"]["HMHY"];
+document.getElementById('M1a').innerHTML = Object.values(Zaka)[0]["Hinatazaka46"]["Generation1"]["HSMM"];
+document.getElementById('M1b').innerHTML = Object.values(Zaka)[0]["Hinatazaka46"]["Generation1"]["IGCM"];
+document.getElementById('M1c').innerHTML = Object.values(Zaka)[0]["Hinatazaka46"]["Generation1"]["KGYK"];
+document.getElementById('M1d').innerHTML = Object.values(Zaka)[0]["Hinatazaka46"]["Generation1"]["KZMM"];
+document.getElementById('M1e').innerHTML = Object.values(Zaka)[0]["Hinatazaka46"]["Generation1"]["KTSH"];
+document.getElementById('M1f').innerHTML = Object.values(Zaka)[0]["Hinatazaka46"]["Generation1"]["SSKM"];
+document.getElementById('M1g').innerHTML = Object.values(Zaka)[0]["Hinatazaka46"]["Generation1"]["SSMR"];
+document.getElementById('M1h').innerHTML = Object.values(Zaka)[0]["Hinatazaka46"]["Generation1"]["STKK"];
+document.getElementById('M1i').innerHTML = Object.values(Zaka)[0]["Hinatazaka46"]["Generation1"]["TKAK"];
+document.getElementById('M1j').innerHTML = Object.values(Zaka)[0]["Hinatazaka46"]["Generation1"]["TKNN"];
+document.getElementById('M1k').innerHTML = Object.values(Zaka)[0]["Hinatazaka46"]["Generation1"]["USSN"];
 }).catch((error) => {
 console.log(error);
 });
-}
+};
+
+const HG2 = () => {
+fetch(url).then((res) => {
+  return res.json();
+  var Zaka = JSON.stringify(url);
+}).then((Zaka) => {
+//document.getElementById('memb').innerHTML = JSON.stringify(Zaka);
+console.log(Object.values(Zaka)[0]["Keyakizaka46"]["Generation1"]);
+document.getElementById('M2a').innerHTML = Object.values(Zaka)[0]["Hinatazaka46"]["Generation2"]["HMHY"];
+document.getElementById('M2b').innerHTML = Object.values(Zaka)[0]["Hinatazaka46"]["Generation2"]["KMMK"];
+document.getElementById('M2c').innerHTML = Object.values(Zaka)[0]["Hinatazaka46"]["Generation2"]["KWHN"];
+document.getElementById('M2d').innerHTML = Object.values(Zaka)[0]["Hinatazaka46"]["Generation2"]["KSKN"];
+document.getElementById('M2e').innerHTML = Object.values(Zaka)[0]["Hinatazaka46"]["Generation2"]["MTKN"];
+document.getElementById('M2f').innerHTML = Object.values(Zaka)[0]["Hinatazaka46"]["Generation2"]["MYMN"];
+document.getElementById('M2g').innerHTML = Object.values(Zaka)[0]["Hinatazaka46"]["Generation2"]["NBAK"];
+document.getElementById('M2h').innerHTML = Object.values(Zaka)[0]["Hinatazaka46"]["Generation2"]["TMSK"];
+document.getElementById('M2i').innerHTML = Object.values(Zaka)[0]["Hinatazaka46"]["Generation2"]["WTMH"];
+}).catch((error) => {
+console.log(error);
+});
+};
+
+const HG3 = () => {
+fetch(url).then((res) => {
+  return res.json();
+  var Zaka = JSON.stringify(url);
+}).then((Zaka) => {
+//document.getElementById('memb').innerHTML = JSON.stringify(Zaka);
+console.log(Object.values(Zaka)[0]["Keyakizaka46"]["Generation1"]);
+document.getElementById('M3a').innerHTML = Object.values(Zaka)[0]["Hinatazaka46"]["Generation3"]["KMMH"];
+}).catch((error) => {
+console.log(error);
+});
+};
--- js/Keyakizaka46.js
+++ js/Keyakizaka46.js
@@ -5,8 +5,49 @@ fetch(url).then((res) => {
 }).then((Zaka) => {
 //document.getElementById('memb').innerHTML = JSON.stringify(Zaka);
 console.log(Object.values(Zaka)[0]["Keyakizaka46"]["Generation1"]);
-document.getElementById('M1a').innerHTML = Object.values(Zaka)[0]["Keyakizaka46"]["Generation1"]["KMM"];
-document.getElementById('M2b').innerHTML = Object.values(Zaka)[0]["Keyakizaka46"]["Generation2"]["FJK"];
+document.getElementById('M1a').innerHTML = Object.values(Zaka)[0]["Keyakizaka46"]["Generation1"]["HMH"];
+document.getElementById('M1b').innerHTML = Object.values(Zaka)[0]["Keyakizaka46"]["Generation1"]["HAO"];
+document.getElementById('M1c').innerHTML = Object.values(Zaka)[0]["Keyakizaka46"]["Generation1"]["HMY"];
+document.getElementById('M1d').innerHTML = Object.values(Zaka)[0]["Keyakizaka46"]["Generation1"]["HYN"];
+document.getElementById('M1e').innerHTML = Object.values(Zaka)[0]["Keyakizaka46"]["Generation1"]["IYU"];
+document.getElementById('M1f').innerHTML = Object.values(Zaka)[0]["Keyakizaka46"]["Generation1"]["INK"];
+document.getElementById('M1g').innerHTML = Object.values(Zaka)[0]["Keyakizaka46"]["Generation1"]["KYU"];
+document.getElementById('M1h').innerHTML = Object.values(Zaka)[0]["Keyakizaka46"]["Generation1"]["KMM"];
+document.getElementById('M1i').innerHTML = Object.values(Zaka)[0]["Keyakizaka46"]["Generation1"]["MAK"];
+document.getElementById('M1j').innerHTML = Object.values(Zaka)[0]["Keyakizaka46"]["Generation1"]["NNK"];
+document.getElementById('M1k').innerHTML = Object.values(Zaka)[0]["Keyakizaka46"]["Generation1"]["ONN"];
+document.getElementById('M1l').innerHTML = Object.values(Zaka)[0]["Keyakizaka46"]["Generation1"]["ORK"];
+document.getElementById('M1m').innerHTML = Object.values(Zaka)[0]["Keyakizaka46"]["Generation1"]["SFK"];
+document.getElementById('M1n').innerHTML = Object.values(Zaka)[0]["Keyakizaka46"]["Generation1"]["SSR"];
+document.getElementById('M1o').innerHTML = Object.values(Zaka)[0]["Keyakizaka46"]["Generation1"]["SMK"];
+document.getElementById('M1p').innerHTML = Object.values(Zaka)[0]["Keyakizaka46"]["Generation1"]["SYK"];
+document.getElementById('M1q').innerHTML = Object.values(Zaka)[0]["Keyakizaka46"]["Generation1"]["SMH"];
+document.getElementById('M1r').innerHTML = Object.values(Zaka)[0]["Keyakizaka46"]["Generation1"]["SMY"];
+document.getElementById('M1s').innerHTML = Object.values(Zaka)[0]["Keyakizaka46"]["Generation1"]["URN"];
+document.getElementById('M1t').innerHTML = Object.values(Zaka)[0]["Keyakizaka46"]["Generation1"]["WRK"];
+document.getElementById('M1u').innerHTML = Object.values(Zaka)[0]["Keyakizaka46"]["Generation1"]["WRS"];
+document.getElementById('M1v').innerHTML = Object.values(Zaka)[0]["Keyakizaka46"]["Generation1"]["YNN"];
+}).catch((error) => {
+console.log(error);
+});
+};
+
+const KG2 = () => {
+fetch(url).then((res) => {
+  return res.json();
+  var Zaka = JSON.stringify(url);
+}).then((Zaka) => {
+//document.getElementById('memb').innerHTML = JSON.stringify(Zaka);
+console.log(Object.values(Zaka)[0]["Keyakizaka46"]["Generation1"]);
+document.getElementById('M2a').innerHTML = Object.values(Zaka)[0]["Keyakizaka46"]["Generation2"]["FJK"];
+document.getElementById('M2b').innerHTML = Object.values(Zaka)[0]["Keyakizaka46"]["Generation2"]["IRN"];
+document.getElementById('M2c').innerHTML = Object.values(Zaka)[0]["Keyakizaka46"]["Generation2"]["MDR"];
+document.getElementById('M2d').innerHTML = Object.values(Zaka)[0]["Keyakizaka46"]["Generation2"]["MTR"];
+document.getElementById('M2e').innerHTML = Object.values(Zaka)[0]["Keyakizaka46"]["Generation2"]["MHK"];
+document.getElementById('M2f').innerHTML = Object.values(Zaka)[0]["Keyakizaka46"]["Generation2"]["SKY"];
+document.getElementById('M2g').innerHTML = Object.values(Zaka)[0]["Keyakizaka46"]["Generation2"]["TKY"];
+document.getElementById('M2h').innerHTML = Object.values(Zaka)[0]["Keyakizaka46"]["Generation2"]["TMH"];
+document.getElementById('M2i').innerHTML = Object.values(Zaka)[0]["Keyakizaka46"]["Generation2"]["TMT"];
 }).catch((error) => {
 console.log(error);
 });
